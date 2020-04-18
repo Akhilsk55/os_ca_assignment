470fe4e1bce03ca1434324cb6c585d66ccd1addd
Akhilsk55 <54187042+Akhilsk55@users.noreply.github.com>
2020-04-18 13:17:31 +0530

    Update SJF_scheduling.c

--- SJF_scheduling.c
+++ SJF_scheduling.c
@@ -12,25 +12,15 @@ struct process p;
 int i, j;
 for(i=0;i<n-1;i++)
 {
-
 for(j=i+1;j<n;j++)
-
 {
-
 if(p_list[i].arrival_t > p_list[j].arrival_t)
-
 {
-
 p = p_list[i];
-
 p_list[i] = p_list[j];
-
 p_list[j] = p;
-
 }
-
 }
-
 }
  }
  int main()
