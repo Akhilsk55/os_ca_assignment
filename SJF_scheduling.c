74ef7f74326dfb0c22412f5b950cccf57b5b1b8c
Akhilsk55 <54187042+Akhilsk55@users.noreply.github.com>
2020-04-18 13:18:00 +0530

    Update SJF_scheduling.c

--- SJF_scheduling.c
+++ SJF_scheduling.c
@@ -25,23 +25,14 @@ p_list[j] = p;
  }
  int main()
  {
-
 int i,t=0,b_t=0,peak;
-
 int a[10];
-
 float wait_time = 0, taround_time = 0, avg_w_t=0, avg_taround_time=0;
-
 printf("enter the no. of processes: ");
-
 scanf("%d",&n);
-
 for(i = 0; i < n; i++)
-
 {
-
 p_list[i].p_no = i+1;
-
 printf("\nEnter Details For P%d process:-\n", p_list[i].p_no);
 printf("Enter Arrival Time: ");
 scanf("%d", &p_list[i].arrival_t );
